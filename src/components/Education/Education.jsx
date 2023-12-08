5d1a8109451ddef0cfe77f435ddbf3160d8757cd
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-12-08 23:57:59 +0600

    education section completed

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -1,14 +1,49 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import SectionTitle from "../SectionTitle/SectionTitle";
 
-
 const Education = () => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const response = await axios.get('/public/education.json');
+                setData(response.data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false);
+            }
+        }
+
+        fetchData();
+    }, []); // Empty dependency array means this effect will run once, similar to componentDidMount
+
     return (
         <div className="h-[100vh]" id="education">
-            <SectionTitle heading={'Education'}></SectionTitle>
-            <div className="border-2 md:h-[55vh] mx-20 rounded-lg border-yellow-600">
-<div className="grid grid-cols-1 lg:grid-cols-2">
-
-</div>
+            <SectionTitle heading={'Education'} />
+            <div className="shadow-amber-300 shadow-md border-2 md:h-[68vh] flex justify-center items-center mx-20 rounded-lg border-yellow-600">
+                {loading ? (
+                    <p>Loading...</p>
+                ) : (
+                    <div className="grid grid-cols-2 w-[80%] mx-auto gap-6  "> {/* Fixed typo here */}
+                        {data &&
+                            data.map((item, index) => (
+                                <div key={index} className="">
+                                    <div className="text-white space-y-2 flex  flex-col justify-center">
+                                        <h3 className="text-xl font-bold text-amber-500">{item.degree}</h3>
+                                        <h3>{item.subjects}</h3>
+                                        <h3>{item.institute_name}</h3>
+                                        <h3>{item.result} out of {item.out_of}</h3>
+                                        <h3>{item.passing_year}</h3>
+                                    </div>
+                                  
+                                </div>
+                            ))}
+                    </div>
+                )}
             </div>
         </div>
     );
